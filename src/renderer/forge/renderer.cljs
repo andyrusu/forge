5db50d9600a8ebbc7cfdf72a48b001c51262d92c
Andy Rusu <rusu.andy1@gmail.com>
2016-04-10 13:40:16 +0300

    backing up old code

--- src/renderer/forge/renderer.cljs
+++ src/renderer/forge/renderer.cljs
@@ -1,12 +1,1 @@
-(ns forge.renderer
-  (:require [forge.ui.window :as w]
-            [common.cards :as cards]
-            [reagent.core :as r]))
-
-(def window (r/atom {:cards [(cards/make-value-card 4)]}))
-
-(defn main
-  []
-  (w/init-gui window))
-
-(main)
+(ns forge.renderer)
